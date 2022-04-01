bfe051a0151a1f1159715031e5a388e0978229a9
Zion <122zmiller@frhsd.com>
2022-04-01 13:26:57 -0400

    testing1

--- site.js
+++ site.js
@@ -34,7 +34,7 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-            title: 'IDMB + Zions Top 7 Movies',
+            title: 'IDMB + Zionss Top 7 Movies',
             owner: 'Zion',
             github: 'https://122zion.github.io/Miller-P3/'
       }
